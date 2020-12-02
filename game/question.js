182a7abb6eff18c6592a52c3e1b010872125287a
Vitor Prim <59850339+lordprim@users.noreply.github.com>
2020-12-02 00:56:31 -0300

    Add files via upload

---
+++ game/question.js
@@ -0,0 +1,30 @@
+
+// array of objects
+const quiz = [
+    {
+        q:'Quantas porcento da população global é prejudicada pela escassez de água? ',
+        options: ['50%', '20%', '40%', '70%'],
+        answer:2
+    },
+    {
+        q:'O que é o lixo? ',
+        options: ['Tudo que o homem descarta', '', 'Florianópolis', 'Rio de Janeiro'],
+        answer:0
+    },
+    {
+        q:'Em qual ano foi confirmado o primeiro caso da doença ?',
+        options: ['2017', '2018', '2020', '2019'],
+        answer:3
+    },
+    {
+        q:'Qual desses sintomas o paciente do COVID-19 sente?',
+        options: ['Dor no polegar', 'Dor nas costas', 'Febre', 'Formigação na mão'],
+        answer:2
+    },
+    {
+        q:'Quem é mais propissio a pegar COVID ?',
+        options: ['Recém-nascidos', 'Adolescentes', 'Crianças', 'Idosos'],
+        answer:3
+    }
+
+]
